d861180071853dff0124b9dc6460fdd8c9fdb790
Sara Vieira <hey@iamsaravieira.com>
2019-01-07 13:29:37 +0100

    simnplify code

--- src/consts.js
+++ src/consts.js
@@ -1,54 +1,52 @@
 export default {
-  twitter: (link, message) =>
+  twitter: (link = '', message = '') =>
     `https://twitter.com/intent/tweet/?text=${encodeURIComponent(
-      message || ''
-    )}&url=${encodeURIComponent(link || '')}`,
-  facebook: link =>
-    `https://facebook.com/sharer/sharer.php?u=${encodeURIComponent(
-      link || ''
-    )}`,
-  google: link =>
-    `https://plus.google.com/share?url=${encodeURIComponent(link || '')}`,
-  tumblr: link =>
+      message
+    )}&url=${encodeURIComponent(link)}`,
+  facebook: (link: '') =>
+    `https://facebook.com/sharer/sharer.php?u=${encodeURIComponent(link)}`,
+  google: (link: '') =>
+    `https://plus.google.com/share?url=${encodeURIComponent(link)}`,
+  tumblr: (link: '') =>
     `http://tumblr.com/widgets/share/tool?canonicalUrl=${encodeURIComponent(
-      link || ''
+      link
     )}`,
-  reddit: link =>
-    `https://reddit.com/submit/?url=${encodeURIComponent(link || '')}`,
-  whatsapp: (link, message) =>
+  reddit: (link: '') =>
+    `https://reddit.com/submit/?url=${encodeURIComponent(link)}`,
+  whatsapp: (link = '', message = '') =>
     `whatsapp://send?text=${encodeURIComponent(message)}%20${encodeURIComponent(
-      link || ''
+      link
     )}`,
-  telegram: (link, message) =>
+  telegram: (link = '', message = '') =>
     `https://telegram.me/share/url?text=${encodeURIComponent(
-      message || ''
-    )}&url=${encodeURIComponent(link || '')}`,
-  vk: (link, message) =>
+      message
+    )}&url=${encodeURIComponent(link)}`,
+  vk: (link = '', message = '') =>
     `http://vk.com/share.php?title=${encodeURIComponent(
-      message || ''
-    )}&url=${encodeURIComponent(link || '')}`,
-  hacker: (link, message) =>
+      message
+    )}&url=${encodeURIComponent(link)}`,
+  hacker: (link = '', message = '') =>
     `https://news.ycombinator.com/submitlink?u=${encodeURIComponent(
-      link || ''
+      link
     )}&t=${encodeURIComponent(message)}`,
-  xing: (link, message) =>
+  xing: (link = '', message = '') =>
     `https://www.xing.com/app/user?op=share;url=${encodeURIComponent(
-      link || ''
+      link
     )};title=${encodeURIComponent(message)}.`,
-  mail: (link, subject, body) =>
+  mail: (link = '', subject, body) =>
     `mailto:?subject=${encodeURIComponent(
       subject || ''
-    )}&body=${encodeURIComponent(body || link || '')}`,
-  pinterest: (link, message) =>
+    )}&body=${encodeURIComponent((body && `${body}\n\n${link}`) || link)}`,
+  pinterest: (link = '', message = '') =>
     `https://pinterest.com/pin/create/button/?url=${encodeURIComponent(
-      link || ''
-    )}&media=${encodeURIComponent(link || '')}&description=${encodeURIComponent(
-      message || ''
+      link
+    )}&media=${encodeURIComponent(link)}&description=${encodeURIComponent(
+      message
     )}`,
-  linkedin: (link, message) =>
+  linkedin: (link = '', message = '') =>
     `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(
-      link || ''
-    )}&title=${encodeURIComponent(message || '')}&summary=${encodeURIComponent(
-      message || ''
-    )}&source=${encodeURIComponent(link || '')}`
+      link
+    )}&title=${encodeURIComponent(message)}&summary=${encodeURIComponent(
+      message
+    )}&source=${encodeURIComponent(link)}`
 }
